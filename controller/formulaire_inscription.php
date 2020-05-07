76c506a741ea5ea709ace02ba569e532076f36c9
Hien Pham <daisypham@Hiens-MacBook-Pro-3.local>
2020-05-07 16:55:56 +0200

    corriger les points

--- controller/formulaire_inscription.php
+++ controller/formulaire_inscription.php
@@ -1,6 +1,6 @@
 <?php
 echo "<html><head><title>accueil</title></head><body><table border='0' width='100%' ><tr><th class='left_logo' width='50%' height='650px'>
-<a href='/index.php'><img class='accueil_btn' src='/vue/photo_specific/logo_2.png' width='50%'></a><th>";
+<a href='/index.php'><img class='accueil_btn' src='./vue/photo_specific/logo_2.png' width='50%'></a><th>";
 $erreur ='';
 if(isset($_GET['erreur'])) {
     if ($_GET['erreur'] == "case_vide") {
@@ -13,7 +13,7 @@ if(isset($_GET['erreur'])) {
         $erreur = '';
     }
 }
-echo "<form action='/modele/inscription.php' method='POST'>";
+echo "<form action='./modele/inscription.php' method='POST'>";
 echo "<label class='nom_projet'><h1>I N T ' M A R K E T</h1></label>";
 echo "<p class='p'>Completer pour nous rejoindre, s'il vous plaît!</p>";
 echo "<h4 class='erreur'>$erreur</h4>";
@@ -26,6 +26,6 @@ echo "<input type='password' name='comf_mdp' placeholder='Comfirmer mot de pass'
 echo "<br><input type='submit' name='valider' value='valider' class='valider_btn'>";
 echo "</form>";
 echo "<br><br><br>
-       <a href='/index.php?action=formulaire_connexion' class='retourner_btn'>Avez-vous déjà une compte? Connectez-vous.</a></form>";
+       <a href='./index.php?action=formulaire_connexion' class='retourner_btn'>Avez-vous déjà une compte? Connectez-vous.</a></form>";
 echo "</tr></table></body></html>";
 ?>
--- controller/profil_utilisateur_courant.php
+++ controller/profil_utilisateur_courant.php
@@ -1,10 +1,11 @@
 <?php
 session_start();
+var_dump($_SESSION);
 if (isset($_GET)) {
     if ($_GET['action'] == "deconnecter") {
         session_destroy();
         header("location: ../index.php");
-    } elseif ($_GET['action'] == "votre_profil") {
+    } elseif ($_GET['action'] == "votre_compte") {
         if (!isset($_SESSION['idUtilisateur'])) {
             header("location: ../index.php?action=formulaire_connexion&erreur=reconnexion");
         } else {
