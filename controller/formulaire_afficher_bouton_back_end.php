de535b5c2d9a37de0ed46818ca3cb2a1a1d90a61
edahan27 <edahan@intechinfo.fr>
2020-05-19 11:21:08 +0200

    ps

--- controller/formulaire_afficher_bouton_back_end.php
+++ controller/formulaire_afficher_bouton_back_end.php
@@ -4,13 +4,13 @@
         $idUtilisateur = $_SESSION['idUtilisateur'];
         $reponde = $bdd->query("SELECT * FROM intmarket.utilisateur");
         while ($trouve = $reponde -> fetch()){
-            if($idUtilisateur == $trouve['idUtilisateur'] && $trouve['role'] == "aministrateur"){
+            if($idUtilisateur == $trouve['idUtilisateur'] && $trouve['role'] == "administrateur"){
                 echo "<div class=\"dropdown\">
-                            <button class=\"dropbtn\">BACK END  <img src=\"/vue/photo_specific/maison_loisirs.png\" width=\"30px\" height=\"30px\">
+                            <button class=\"dropbtn\">BACK END  <img src=\"./vue/photo_specific/maison_loisirs.png\" width=\"30px\" height=\"30px\">
                             <i class=\"fa fa-caret-down\"></i>
                         </button>
                             <div class=\"dropdown-content\">
-                                <a href=\"/index.php?action=back_endd\">test 2</a>
+                                <a href=\"./index.php?action=back_endd\">test 2</a>
 
                             </div>
                         </div>";
