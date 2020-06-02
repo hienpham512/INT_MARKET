a9fc5fe924b18f68675799bbb5182c9d492a433c
Sekou <amtraore@intechinfo.fr>
2020-06-02 16:26:15 +0200

    footer

---
+++ modele/footer.php
@@ -0,0 +1,53 @@
+    <!DOCTYPE html>
+        <html>
+          
+            <title>W3.CSS Template</title>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+            <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+            <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Karma">
+            <style>
+            body,h1,h2,h3,h4,h5,h6 {font-family: "Karma", sans-serif}
+            .w3-bar-block .w3-bar-item {padding:20px}
+            </style>
+
+            <head>
+                <meta charset="UTF-8">
+                <title>INTMARKET accueil</title>
+            </head>
+
+            <body>
+                  <!-- Footer -->
+  <footer class="w3-row-padding w3-padding-32">
+    <div class="w3-third">
+      <h3>INT'MARKET</h3>
+      <p>   INT’MARKET est un site d’achat en ligne. </p>                                           
+      <p>  INT’MARKET vous permet de faire des achats de tous genres, à tous moments, en un clic et sans bouger de chez vous. Vous y trouverez tout ce dont vous avez besoin allant de l’électronique aux vêtements et accessoires à des prix très raisonnables et une excellente  qualité de service.
+        </p>
+     
+    </div>
+  
+    <div class="w3-third">
+      <h3>BLOG POSTS</h3>
+        <div>
+            <a href="./index.php" class="photo_logo"><img src='./vue/photo_specific/logo.png' width='110px' height="110px"></a>
+            <img src="./vue/photo_specific/instagram.png" width="50px" height="50px">
+            <img src="./vue/photo_specific/twitter.png" width="50px" height="50px">
+           <img src="./vue/photo_specific/facebook.png" width="50px" height="50px">
+        </div>
+    </div>
+
+    <div class="w3-third w3-serif">
+      <h3>POPULAR TAGS</h3>
+      <p>
+        <span class="w3-tag w3-black w3-margin-bottom">Alimentation</span> <span class="w3-tag w3-dark-grey w3-small w3-margin-bottom">Salon</span> <span class="w3-tag w3-dark-grey w3-small w3-margin-bottom">Dinner</span>
+        <span class="w3-tag w3-dark-grey w3-small w3-margin-bottom">Légume</span> <span class="w3-tag w3-dark-grey w3-small w3-margin-bottom">boisson</span> <span class="w3-tag w3-dark-grey w3-small w3-margin-bottom">Drinks</span>
+        <span class="w3-tag w3-dark-grey w3-small w3-margin-bottom">Brasserie</span> <span class="w3-tag w3-dark-grey w3-small w3-margin-bottom">Matériaux</span> <span class="w3-tag w3-dark-grey w3-small w3-margin-bottom">Cuisine</span>
+        <span class="w3-tag w3-dark-grey w3-small w3-margin-bottom">Chicken</span> <span class="w3-tag w3-dark-grey w3-small w3-margin-bottom">Dressing</span> <span class="w3-tag w3-dark-grey w3-small w3-margin-bottom">Fried</span>
+        <span class="w3-tag w3-dark-grey w3-small w3-margin-bottom">Fish</span> <span class="w3-tag w3-dark-grey w3-small w3-margin-bottom">Accessoire</span>
+      </p>
+    </div>
+  </footer>
+            </body> 
+        </html>
+  
