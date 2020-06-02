420abb7d097ea32f8d51db3ea8e832ea3ebfb911
Hien Pham <daisypham@Hiens-MacBook-Pro-3.local>
2020-06-02 12:00:18 +0200

    récrire la partie backend

---
+++ controller/formulaire_afficher_renvoie_de_ficher_backend.php
@@ -0,0 +1,15 @@
+<?php
+session_start();
+if (isset($_SESSION['status'])) {
+    if ($_SESSION['status'] == "succes") {
+        $status = "SUCCÈS";
+    } elseif ($_SESSION['status'] == "erreur_mail") {
+        $status = "le mail est invalidé!";
+    } elseif ($_SESSION['status'] == "erreur") {
+        $status = "ERREUR";
+    } else {
+        $status = '';
+    }
+    echo "<label>$status</label>";
+    unset($_SESSION['status']);
+}
