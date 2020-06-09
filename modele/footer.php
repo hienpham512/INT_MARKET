b589a5f8c4de38d195fdd5ddd073412300818412
Sekou <amtraore@intechinfo.fr>
2020-06-09 10:18:27 +0200

    dernière modification avant l interation du 09/06/2020 14h30

--- modele/footer.php
+++ modele/footer.php
@@ -1,53 +1,58 @@
-    <!DOCTYPE html>
-        <html>
-          
-            <title>W3.CSS Template</title>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1">
-            <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-            <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Karma">
-            <style>
-            body,h1,h2,h3,h4,h5,h6 {font-family: "Karma", sans-serif}
-            .w3-bar-block .w3-bar-item {padding:20px}
-            </style>
-
-            <head>
-                <meta charset="UTF-8">
-                <title>INTMARKET accueil</title>
-            </head>
-
-            <body>
-                  <!-- Footer -->
-  <footer class="w3-row-padding w3-padding-32">
-    <div class="w3-third">
-      <h3>INT'MARKET</h3>
-      <p>   INT’MARKET est un site d’achat en ligne. </p>                                           
-      <p>  INT’MARKET vous permet de faire des achats de tous genres, à tous moments, en un clic et sans bouger de chez vous. Vous y trouverez tout ce dont vous avez besoin allant de l’électronique aux vêtements et accessoires à des prix très raisonnables et une excellente  qualité de service.
-        </p>
-     
-    </div>
+<!DOCTYPE html>
+<html>
+<title>W3.CSS Template</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<style>
+.w3-sidebar a {font-family: "Roboto", sans-serif}
+body,h1,h2,h3,h4,h5,h6,.w3-wide {font-family: "Montserrat", sans-serif;}
+</style>
+<body class="w3-content" style="max-width:1200px">
   
-    <div class="w3-third">
-      <h3>BLOG POSTS</h3>
-        <div>
-            <a href="./index.php" class="photo_logo"><img src='./vue/photo_specific/logo.png' width='110px' height="110px"></a>
-            <img src="./vue/photo_specific/instagram.png" width="50px" height="50px">
-            <img src="./vue/photo_specific/twitter.png" width="50px" height="50px">
-           <img src="./vue/photo_specific/facebook.png" width="50px" height="50px">
-        </div>
-    </div>
+  <!-- Footer -->
+  <footer class="w3-padding-64 w3-light-grey w3-small w3-center" id="footer">
+    <div class="w3-row-padding">
+      <div class="w3-col s4">
+        <h4>Contact</h4>
+        <p>Des questions? Allez y.</p>
+        <form action="./index.php" method="POST">
+          <p><input class="w3-input w3-border" type="text" placeholder="Name" name="Name" required></p>
+          <p><input class="w3-input w3-border" type="text" placeholder="Email" name="Email" required></p>
+          <p><input class="w3-input w3-border" type="text" placeholder="Subject" name="Subject" required></p>
+          <p><input class="w3-input w3-border" type="text" placeholder="Message" name="Message" required></p>
+          <button type="submit" class="w3-button w3-block w3-black">Send</button>
+        </form>
+      </div>
 
-    <div class="w3-third w3-serif">
-      <h3>POPULAR TAGS</h3>
-      <p>
-        <span class="w3-tag w3-black w3-margin-bottom">Alimentation</span> <span class="w3-tag w3-dark-grey w3-small w3-margin-bottom">Salon</span> <span class="w3-tag w3-dark-grey w3-small w3-margin-bottom">Dinner</span>
-        <span class="w3-tag w3-dark-grey w3-small w3-margin-bottom">Légume</span> <span class="w3-tag w3-dark-grey w3-small w3-margin-bottom">boisson</span> <span class="w3-tag w3-dark-grey w3-small w3-margin-bottom">Drinks</span>
-        <span class="w3-tag w3-dark-grey w3-small w3-margin-bottom">Brasserie</span> <span class="w3-tag w3-dark-grey w3-small w3-margin-bottom">Matériaux</span> <span class="w3-tag w3-dark-grey w3-small w3-margin-bottom">Cuisine</span>
-        <span class="w3-tag w3-dark-grey w3-small w3-margin-bottom">Chicken</span> <span class="w3-tag w3-dark-grey w3-small w3-margin-bottom">Dressing</span> <span class="w3-tag w3-dark-grey w3-small w3-margin-bottom">Fried</span>
-        <span class="w3-tag w3-dark-grey w3-small w3-margin-bottom">Fish</span> <span class="w3-tag w3-dark-grey w3-small w3-margin-bottom">Accessoire</span>
-      </p>
+      <div class="w3-col s4">
+        <h4>A propos</h4>
+        <p><a href="#">Service Clients</a></p>
+        <p><a href="#">Aide</a></p>
+        <p><a href="#">Magasins</a></p>
+        <p><a href="#">Mentions légales et confidentialité</a></p>
+      </div>
+
+      <div class="w3-col s4 w3-justify">
+        <h4>Magasins</h4>
+        <p><a href="https://www.google.com/search?q=intech&rlz=1C1CHBF_frFR842FR842&oq=intech&aqs=chrome.0.69i59j0l4j69i60l3.4713j0j7&sourceid=chrome&ie=UTF-8&sxsrf=ALeKk022dGY4piIBRkwWUagSNcAWqBgtBA:1591618174373&npsic=0&rflfq=1&rlha=0&rllag=48854940,2356447,10748&tbm=lcl&rldimm=2523522275773450028&lqi=CgZpbnRlY2haEAoGaW50ZWNoIgZpbnRlY2g&ved=2ahUKEwi625rfl_LpAhVQ1hoKHeLiBcAQvS4wAHoECAsQIQ&rldoc=1&tbs=lrf:!1m4!1u3!2m2!3m1!1e1!1m4!1u2!2m2!2m1!1e1!2m1!1e2!2m1!1e3!3sIAE,lf:1,lf_ui:2&rlst=f#rlfi=hd:;si:17527167201580339468,l,CgZpbnRlY2haEAoGaW50ZWNoIgZpbnRlY2g;mv:[[52.827741499999995,22.2009075],[44.7660202,-1.1940199999999999]]"><i class="fa fa-fw fa-map-marker"></i> INT'MARKET</p></a>
+        <p><i class="fa fa-fw fa-phone"></i> 0033753522072</p>
+        <p><i class="fa fa-fw fa-envelope"></i> intmarket@gmail.com</p>
+        <h4>Paiement</h4>
+        <p><i class="fa fa-fw fa-cc-amex"></i> Paypal</p>
+        <p><i class="fa fa-fw fa-credit-card"></i> Credit Card</p>
+        <br>
+        <a href="https://www.facebook.com/Intmarket-110395434038948/"><i class="fa fa-facebook-official w3-hover-opacity w3-large"></i></a>
+        <a href="https://www.instagram.com/intmarket1/"><i class="fa fa-instagram w3-hover-opacity w3-large"></i></a>
+        <i class="fa fa-snapchat w3-hover-opacity w3-large"></i>
+        <i class="fa fa-twitter w3-hover-opacity w3-large"></i>
+      </div>
     </div>
   </footer>
-            </body> 
-        </html>
-  
+  <!-- End page content -->
+</div>
+</body>
+</html>
