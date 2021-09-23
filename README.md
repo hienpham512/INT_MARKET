bb1802c20fd0ca32e051adb9524e9b5408b021f7
thi-hien.pham <thi-hien.pham@epitech.eu>
2021-09-23 15:39:10 +0200

    [DEL/MOD]

--- README.md
+++
@@ -1,2 +0,0 @@
-# INT_MARKET
-INT’MARKET est un site d’achat en ligne.                                              Le projet consiste à réaliser un site web d’achat en ligne revisité. Le but étant de pouvoir pour l’utilisateur d’acheter tout et n’importe quoi, vêtement, électronique, aliments etc. ainsi les clients  repartirez avec tout leur besoin ainsi que le sourire.
--- controller/fct_analyser_mdp.php
+++ controller/fct_analyser_mdp.php
@@ -5,13 +5,6 @@ function verifier_mdp($mdp){
         $erreur = false;
         $_SESSION['erreur'] = "moin_de_6_carractere";
     }
-    /*$mdp = str_split(htmlspecialchars($mdp));
-    $replace_special_carractere = array("á","â", "à", "å", "ä", "é", "ê", "è", "ë", "ì", "í", "î", "ï", "ô", "ö", "ñ", "ò", "ó", "õ", "ø", "œ", "š", "ç", "ù", "û", "ú", "ü");
-    foreach ($replace_special_carractere as $key => $value) {
-        if (in_array($value, $mdp)) {
-            $erreur = "carractere_special";
-        }
-    }*/
     return $erreur;
 }
 function chiffrer_mdp($mdp){
